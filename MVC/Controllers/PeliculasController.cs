8bea471f11d2adc4bda1bf53adf302b9c6c35e93
Axel GabrielMolaro <axelmolaro3@gmail.com>
2017-05-04 01:31:06 -0300

    2 vistas mas

---
+++ MVC/Controllers/PeliculasController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        // GET: Peliculas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult reserva()
+        {
+            return View();
+        }
+        
+        //[HttpPost]
+        public ActionResult reserva2()
+        {
+            return View();
+        }
+    }
+}
